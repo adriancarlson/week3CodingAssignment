4006695a5912e4a91d192ac94cd1bf7820835135
adriancarlson <adrian.r.carlson@gmail.com>
2021-04-08 21:45:49 -0500

    third push

--- index.js
+++ index.js
@@ -3,63 +3,52 @@ let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 // a.	Programmatically subtract the value of the first element in the array from the value in the last element of the array (do not use numbers to reference the last element, find it programmatically, ages[7] – ages[0] is not allowed). Print the result to the console.
 
-function firstFromLast(array) {
-	let firstAge = array[0];
-	let lastAge = array[array.length - 1];
-	return lastAge - firstAge;
-}
-console.log(firstFromLast(ages));
+let firstAge = ages[0];
+// let firstAge = ages.shift(); cannot use because it actually removes the first element from the array which we need all elements for later functions
+let lastAge = ages[ages.length - 1];
+// let lastAge = ages.pop(); cannot use because it actually removes the first element from the array which we need all elements for later functions
+let firstFromLast = lastAge - firstAge;
+
+console.log('1a. result: ', firstFromLast);
 
 // b.	Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
 
 ages.push(33);
-console.log(firstFromLast(ages));
 
 // c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
 
-function findAvg(array) {
-	let sum = 0;
-	let total = array.length;
-	for (element of array) {
-		sum += element;
-	}
-	let avg = sum / total;
-	return avg;
+let sum = 0;
+let total = ages.length;
+for (age of ages) {
+	sum += age;
 }
+let avg = sum / total;
 
-console.log(findAvg(ages));
+console.log('1c. result: ', avg);
 
 // 2.	Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
 
 // a.	Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
+// let nameLengths = names.map(function (name) {
+// 	return name.length;
+// });
 
-function avgLettersPerName(array) {
-	let arrayTotal = array.length;
-
-	let lengths = array.map(function (element) {
-		return element.length;
-	});
-	let sum = 0;
-	for (element of lengths) {
-		sum += element;
-	}
-	let avg = sum / arrayTotal;
-	return avg;
+let letterSum = 0;
+let letterTotal = names.length;
+for (name of names) {
+	letterSum += name.length;
 }
-console.log(avgLettersPerName(names));
+let letterAvg = letterSum / letterTotal;
+
+console.log('2a. result: ', letterAvg);
 
 // b.	Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
-
-function concatenateNames(array) {
-	namesString = '';
-	for (element of array) {
-		namesString += element + ' ';
-	}
-	return namesString.trim();
+namesString = '';
+for (name of names) {
+	namesString += name + ' ';
 }
-
-console.log(concatenateNames(names));
+console.log('2b. result: ', namesString.trim());
 
 // 3.	How do you access the last element of any array?
 // array[array.length - 1];
@@ -73,17 +62,15 @@ let nameLengths = names.map(function (element) {
 	return element.length;
 });
 
-// console.log(nameLengths);
+// console.log('5. result: ', nameLengths);
+
 // 6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
-function sumNameLengths(array) {
-	let sum = 0;
-	for (element of array) {
-		sum += element;
-	}
-	return sum;
+let nameLengthSum = 0;
+for (nameLength of nameLengths) {
+	nameLengthSum += nameLength;
 }
-sumNameLengths(nameLengths);
-// console.log(sumNameLengths(nameLengths));
+
+// console.log('6. result: ', nameLengthSum);
 
 // 7.	Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
 
@@ -95,13 +82,14 @@ function concatenateWords(word, n) {
 	return finalString;
 }
 concatenateWords('Hello', 3);
-// console.log(concatenateWords('Hello', 3));
+// console.log('7. result: ', concatenateWords('Hello', 3));
+
 // 8.	Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
 function fullName(firstName, lastName) {
 	return firstName + ' ' + lastName;
 }
 fullName('Adrian', 'Carlson');
-// console.log(fullName('Adrian', 'Carlson'));
+// console.log('8. result: ', fullName('Adrian', 'Carlson'));
 
 // 9.	Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
 testNumbers = [2, 3, 4, 5, 6, 7, 8, 19, 33];
@@ -115,8 +103,10 @@ function isGreaterThanAHundy(array) {
 		return false;
 	}
 }
-console.log(isGreaterThanAHundy(ages));
+isGreaterThanAHundy(testNumbers);
+// console.log('9. result: ', isGreaterThanAHundy(testNumbers));
 // 10.	Write a function that takes an array of numbers and returns the average of all the elements in the array.
+
 function findAvg(array) {
 	let sum = 0;
 	let total = array.length;
@@ -126,8 +116,8 @@ function findAvg(array) {
 	let avg = sum / total;
 	return avg;
 }
-
-// console.log(findAvg(ages));
+findAvg(testNumbers);
+// console.log('10. result: ', findAvg(testNumbers));
 
 // 11.	Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 function testArrayAvgs(array1, array2) {
@@ -149,8 +139,8 @@ function testArrayAvgs(array1, array2) {
 		return false;
 	}
 }
-
-// console.log(testArrayAvgs(ages, testNumbers));
+testArrayAvgs(ages, testNumbers);
+// console.log('11. result: ', testArrayAvgs(ages, testNumbers));
 
 // 12.	Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 let isHotOutside = true;
@@ -163,7 +153,8 @@ function willBuyDrink(boolean, number) {
 		return false;
 	}
 }
-console.log(willBuyDrink(isHotOutside, moneyInPocket));
+willBuyDrink(isHotOutside, moneyInPocket);
+// console.log('12. result: ', willBuyDrink(isHotOutside, moneyInPocket));
 
 // 13.	Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
 function canIBuyIt(cost, wallet) {
